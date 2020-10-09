a76296f08635c41f44a4875d88cb81451945f9bf
Wolfgang Pichler <39938946+pichlerx@users.noreply.github.com>
2020-10-09 20:30:44 +0200

    Update update_schillingcoin.sh
    rm update_schillingcoin.sh added

--- update_schillingcoin.sh
+++ update_schillingcoin.sh
@@ -2,6 +2,7 @@
 
 cd 
 schillingcoin-cli stop
+rm update_schillingcoin.sh
 wget http://www.schillingcoin.org/download/SCH_v2.0/SCH-2.0.0-ubuntu-daemon.tar.gz
 tar -xvzf SCH-2.0.0-ubuntu-daemon.tar.gz
 rm /usr/local/bin/schillingcoind && rm /usr/local/bin/schillingcoin-cli
